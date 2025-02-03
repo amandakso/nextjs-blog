e23b0d1eb8e51b2bd6a932068b7b2d0b128a349c
Amanda So <soamanda95@gmail.com>
2025-02-03 10:13:13 -0800

    update title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Amanda's Blog";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Amanda's Blog Site";
 
 export default function Layout({ children, home }) {
   return (
