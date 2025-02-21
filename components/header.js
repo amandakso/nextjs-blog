ab2b53557af0871c9210a7743d4e85863f47ad5c
Amanda So <soamanda95@gmail.com>
2025-02-21 10:17:18 -0800

    add css to header component

--- components/header.js
+++ components/header.js
@@ -1,16 +1,25 @@
 import Link from "next/link";
+import styles from "../styles/utils.module.css";
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <div>
-          <Link href="/">Amanda's Blog Site</Link>
-        </div>
-        <div>
-          <Link href="/">Blog</Link>
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
+    <header className={styles.navHeader}>
+      <nav className={styles.nav}>
+        <div className={styles.navContainer}>
+          <div>
+            <Link href="/">Amanda's Blog Site</Link>
+          </div>
+          <div className={styles.navLinks}>
+            <Link href="/" className={styles.navLink}>
+              Blog
+            </Link>
+            <Link href="/about" className={styles.navLink}>
+              About
+            </Link>
+            <Link href="/contact" className={styles.navLink}>
+              Contact
+            </Link>
+          </div>
         </div>
       </nav>
     </header>
